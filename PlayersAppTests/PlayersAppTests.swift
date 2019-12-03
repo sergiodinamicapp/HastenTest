2b116057718f60de7228b5b9c82d437dc1855713
Sergio Blanco Martin <sergiodinamicapp@gmail.com>
2019-12-03 20:09:32 +0100

    Initial Commit

---
+++ PlayersAppTests/PlayersAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlayersAppTests.swift
+//  PlayersAppTests
+//
+//  Created by sergio blanco martin on 03/12/2019.
+//  Copyright © 2019 sergio blanco martin. All rights reserved.
+//
+
+import XCTest
+@testable import PlayersApp
+
+class PlayersAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
